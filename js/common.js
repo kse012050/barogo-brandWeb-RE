351e236020430a5ee3db5af3ef6aa75303a6077b
김성은 <kse012050@naver.com>
2022-09-03 20:14:14 +0900

    스크롤 중간에 막기 이 방법 밖에 없나?

--- js/common.js
+++ js/common.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
 
     // 슬라이더
     $('*').hasClass('mySwiper') && swiperSlider();
-    $('*').hasClass('mainPage') && fullPage();
+
+    // 풀페이지
+    $('[data-scroll="fullPage"]').length > 0 && fullPage();
+
+    // 회사소개 스크롤 이벤트
+    $('[data-scroll="fixed"]').length > 0 && scrollFix();
 });
 
 function swiperSlider(){
@@ -17,13 +22,16 @@ function swiperSlider(){
 
 function fullPage(){
     $('body').css('overflow','hidden');
+    let scrollAniBool = true;
     $('[data-scroll="fullPage"] > *').on('mousewheel',function(e){
+        if($('html').is(':animated') || $('.progressBar span').is(':animated')) return
         let delta = e.originalEvent.wheelDelta;
         let scrollTopValue;
         let target = $(this).find('[data-scroll="target"]');
         let targetLi = [];
         let targetIdx;
         let targetLength;
+        let progress = $(this).find('.progressBar');
         target.each(function(i){
             targetLi.push(target.eq(i).find('li'))
         })
@@ -34,29 +42,93 @@ function fullPage(){
                 list.eq(i).hasClass('active') && (targetIdx = i);
             })
         })
-        if(delta > 0 && targetIdx > 0){
-            targetIdx--;
-            targetLi.map((t)=>{
-                t.removeClass('active');
-                t.eq(targetIdx).addClass('active');
-            })
-            return
-        }else if(delta < 0 && targetIdx < (targetLength - 1)){
-            targetIdx++;
-            targetLi.map((t)=>{
-                t.removeClass('active');
-                t.eq(targetIdx).addClass('active');
-            })
-            return;
-        }
 
-        if(delta > 0 && (targetIdx == 0 || targetIdx == undefined)){
+        if(delta > 0){
             // 휠을 위로
-            scrollTopValue = $(this).prev().offset().top;
-        }else if(targetIdx == (targetLength - 1) || targetIdx == undefined){
+            (targetIdx > 0) ? scrollAniBool = targetActive(targetLi, --targetIdx , progress) : (scrollTopValue = $(this).prev());
+        }else{
             // 휠을 아래로
-            scrollTopValue = $(this).next().offset().top;
+            (targetIdx < (targetLength - 1)) ? scrollAniBool = targetActive(targetLi, ++targetIdx , progress) : (scrollTopValue = $(this).next());
         }
-        $('html').stop().animate({scrollTop : scrollTopValue});
+        scrollAniBool ? scrollAni(scrollTopValue) : 
+        setTimeout(()=>{
+            scrollAniBool = !scrollAniBool;
+        },500);
+    })
+
+    function scrollAni(scrollTopValue){
+        scrollTopValue.addClass('active').siblings().removeClass('active');
+        $('html').stop().animate({scrollTop : scrollTopValue.offset().top} , 500)
+    }
+
+}
+
+function targetActive(list , idx , progress){
+    progress.find('span').animate({'top':progress.height()  / list[0].length * idx} , 500 , 'linear')
+    list.map((t)=>{
+        t.eq(idx).stop().animate({opacity : 1} , 500).addClass('active').siblings().animate({opacity : 0} , 500).removeClass('active');
+    })
+    return false;
+}
+
+function scrollFix(){
+    $('body').css('overflow','hidden');
+    $('.scrollArea').scroll(function(){
+        $(this).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active');
+        // console.log($('.scrollArea > div').height() - $('.scrollArea').height());
+        // console.log($(this).scrollTop());
+        // if(Math.floor($('.scrollArea > div').height() - $('.scrollArea').height()) <= $(this).scrollTop()){
+           
+        // }
+    })
+
+    $('.toDoArea').on('mousewheel',function(e){
+                
+        if($('.progressBar span').is(':animated')) {
+            e.preventDefault()
+            return
+        }
+        let delta = e.originalEvent.wheelDelta;
+        let target = $(this).find('[data-scroll="target"]');
+        let targetLi = [];
+        let targetIdx;
+        let targetLength;
+        let progress = $(this).find('.progressBar');
+        target.each(function(i){
+            targetLi.push(target.eq(i).find('li'))
+        })
+        targetLi.length > 0 && (targetLength = targetLi[0].length);
+        
+        targetLi.map(function(list){
+            list.each(function(i){
+                list.eq(i).hasClass('active') && (targetIdx = i);
+            })
+        })
+        if(delta > 0){
+            // 휠을 위로
+            // console.log(targetIdx);
+            console.log(Math.floor($('.scrollArea > div').height() - $('.scrollArea').height()) <= $(this).scrollTop());
+            if($('.aboutUsPage').scrollTop() == 0){
+                if(targetIdx > 0){
+                    console.log(1);
+                    e.preventDefault();
+                    scrollAniBool = targetActive(targetLi, --targetIdx , progress)
+                }else{
+                    $('main').removeClass('active');
+                    $('.scrollArea').removeClass('active');
+                }
+            }
+        }else{
+            // 휠을 아래로
+            console.log(Math.floor($('.scrollArea > div').height() - $('.scrollArea').height()) );
+            console.log($('.scrollArea').scrollTop());
+            if(targetIdx < (targetLength - 1) && Math.floor($('.scrollArea > div').height() - $('.scrollArea').height()) <= $('.scrollArea').scrollTop()){ 
+                scrollAniBool = targetActive(targetLi, ++targetIdx , progress)
+            }else if(targetIdx == (targetLength - 1)){
+                $('main').addClass('active');
+                $('.scrollArea').addClass('active');
+
+            }
+        }
     })
 }
