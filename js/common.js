2eed9cda160a51185d840db5f9b9ae7f3046c668
김성은 <kse012050@naver.com>
2022-09-07 05:25:13 +0900

    그래프 만들었다

--- js/common.js
+++ js/common.js
@@ -120,6 +120,7 @@ function scrollAni(list , idx){
 
 function scrollFix(){
     $('body').css('overflow','hidden');
+    canvasInit();
     $('main[data-scroll="area"]').find('[data-scroll="area"]').eq(-1).scroll(function(){
         $(this).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active');
         ($(this).scrollTop() > 0) ? $('.fixedLink').addClass('active') : $('.fixedLink').removeClass('active');
@@ -192,7 +193,8 @@ function scrollFix(){
             if(!$(this).hasClass('active')) return;
             if($('[data-count="area"]').offset().top <= 0 && countBool){
                 countBool = !countBool;
-                 setTimeout(function(){
+                drawScene();
+                setTimeout(function(){
                     countAni($('[data-count="area"] [data-count="target"]'));
                 },1000)
             }
@@ -318,3 +320,134 @@ function bikeAni(){
         targetRight = -(parseInt($('.CW').css('margin-right')) + target.width() - 50);
     }
 }
+
+
+
+// 그래프
+let canvas , ctx , w , h , lines = [];
+function canvasInit(){
+    canvas = document.getElementById('graph');
+    ctx = canvas.getContext('2d');
+    canvasSize();
+    lines.push(new Line());
+    // animationLoop();
+}
+
+function canvasSize(){
+    w = canvas.width = document.querySelector('.graphArea').offsetWidth;
+    h = canvas.height = document.querySelector('.graphArea').offsetHeight;
+}
+
+function drawScene(){
+    animationLoop();
+}
+
+function animationLoop(){
+    ctx.clearRect(0,0,w,h);
+    lines.map((l)=>{
+        l.draw();
+        l.update();
+    })
+    requestAnimationFrame(animationLoop)
+}
+
+class Line{
+    constructor(){
+        this.x = 40;
+        this.y = 329;
+        this.lineX = this.x;
+        this.lineY = this.y;
+        this.progress = 0;
+        this.point = [
+            {x : 40 , y : 329},
+            {x : 139, y : 317},
+            {x : 249, y : 298},
+            {x : 360, y : 261},
+            {x : 469, y : 188},
+            {x : 583, y : 97},
+        ]
+        this.line = [];
+        this.count = 0;
+        this.balls = [new Ball(this.point[0].x , this.point[0].y)];
+        this.lastCheck = false;
+    }
+    draw(){
+        ctx.beginPath();
+        this.line.map((l , i)=>{
+            ctx.moveTo(this.point[i].x , this.point[i].y);
+            ctx.lineTo(l.x , l.y)
+        })
+        ctx.moveTo(this.point[this.count].x , this.point[this.count].y);
+        ctx.lineTo(this.lineX , this.lineY)
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 4
+        ctx.stroke();
+
+        this.balls.map((b)=>{
+            b.draw();
+            b.update();
+        })
+    }
+    update(){
+        if(this.count < this.point.length - 1){ 
+            if(this.count == this.point.length - 2){
+                this.lastCheck = true;
+            }
+            if(this.progress <= 1){
+                this.lineX = this.point[this.count].x + (this.progress * (this.point[this.count + 1].x - this.point[this.count].x))
+                this.lineY = this.point[this.count].y + (this.progress * (this.point[this.count + 1].y - this.point[this.count].y))
+                this.progress += 0.1;
+            }else{
+                this.lineX = this.point[this.count + 1].x;
+                this.lineY = this.point[this.count + 1].y;
+                this.line.push({x : this.lineX , y : this.lineY});
+                this.count++;
+                this.progress = 0;
+                this.balls.push(new Ball(this.lineX , this.lineY , this.lastCheck));
+            }
+        }
+        
+    }
+}
+
+class Ball{
+    constructor(x , y , lastCheck){
+        this.x = x;
+        this.y = y;
+        this.size = 0;
+        this.lastCheck = lastCheck;
+    }
+
+    draw(){
+        if(this.lastCheck){
+            ctx.beginPath();
+            ctx.arc(this.x , this.y , this.size <=19 ? this.size : 19 , 0 , Math.PI * 2 );
+            ctx.fillStyle = 'rgba(250,80,20,1)';
+            ctx.fill()
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.arc(this.x , this.y , this.size , 0 , Math.PI * 2 );
+            ctx.fillStyle = 'rgba(250,80,20,0.5)';
+            ctx.fill()
+        }else{
+            ctx.beginPath();
+            ctx.arc(this.x , this.y , this.size, 0 , Math.PI * 2);
+            ctx.fillStyle = 'black';
+            ctx.fill()
+        }
+    }
+
+    update(){
+        if(this.lastCheck){
+            if(this.size < 45){
+                this.size = this.size + 0.3; 
+            }
+        }else{
+            if(this.size < 8){
+                this.size = this.size + 0.3; 
+            }
+        }
+    }
+}
+
