4b43a74811fd64a68d517aa39ab88aabaf0fcf3a
김성은 <kse012050@naver.com>
2023-08-22 12:12:49 +0900

    개인정보처리방침 에디터 컨텐츠 css ( reset.css ) 수정

---
+++ 서버 데이터 작업/privacy.php
@@ -0,0 +1,22 @@
+<?php include "../head.php"; ?>
+<body>
+
+<div class="policyPage">
+    <?php include "../header.php"; ?>
+    <main class="CW">
+        <!-- <h2>개인정보처리방침</h2> -->
+        <!-- 에디터 기능 contentArea 클래스 추가-->
+        <div class="contentArea">
+            <?=stripslashes(getSiteIntoValue($site,"policy_privacy"))?>
+        </div>
+        <script>
+            $(document).ready(function(){
+                
+            })
+        </script>
+
+    </main>
+    <?php include "../footer.php"; ?>
+</div>
+</body>
+</html>
