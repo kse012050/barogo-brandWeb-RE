6f580886381fcd1a210753480ae7e9c606eb3995
김성은 <kse012050@naver.com>
2023-08-23 20:03:27 +0900

    위치기반서비스이용약관 수정 했다

--- 서버 데이터 백업/locationbased.php
+++ 서버 데이터 백업/locationbased.php
@@ -1,16 +1,16 @@
 <?php include "../head.php"; ?>
 <body>
-
-<div class="policyPage">
+    
+    <div class="policyPage">
     <?php include "../header.php"; ?>
     <main class="CW">
-        <h2>개인정보처리방침</h2>
+        <h2>위치기반서비스이용약관</h2>
         <!-- 에디터 기능 contentArea 클래스 추가-->
         <div class="contentArea">
-            <?=stripslashes(getSiteIntoValue($site,"policy_privacy"))?>
+            <?=stripslashes(getSiteIntoValue($site,"policy_locationbased"))?>
         </div>
     </main>
     <?php include "../footer.php"; ?>
-</div>
+    </div>
 </body>
 </html>
